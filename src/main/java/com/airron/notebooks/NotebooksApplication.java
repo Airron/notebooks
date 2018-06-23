d77cc37ef6e178ef1a6f14343c98d826394f318b
Airron <airron_jee@163.com>
2018-06-23 11:57:51 +0800

    new project

---
+++ src/main/java/com/airron/notebooks/NotebooksApplication.java
@@ -0,0 +1,12 @@
+package com.airron.notebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebooksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/airron/notebooks/NotebooksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.airron.notebooks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotebooksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
